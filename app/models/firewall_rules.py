d159017e878880e39cc6fe982fad6ea7296bc212
MatanAvitan-98 <matan3751@gmail.com>
2021-03-29 20:24:48 +0300

    add firewall rules model

---
+++ app/models/firewall_rules.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from app.models import db
+
+
+class FirewallRules(db.Model):
+    fw_id = db.Column(db.String, primary_key=True)
+    source_tag = db.Column(db.String(100), nullable=False)
+    dest_tag = db.Column(db.String, nullable=False)
+
+    def __init__(self, fw_id, source_tag, dest_tag):
+        self.fw_id = fw_id
+        self.source_tag = source_tag
+        self.dest_tag = dest_tag
